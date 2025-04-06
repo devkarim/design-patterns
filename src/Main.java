82659c55abf9d7d3b09207e8d461c546afa2e6bf
Karim Wael <devkarim@hotmail.com>
2025-04-07 01:02:17 +0200

    refactor: remove unnecessary comments from main class

--- src/Main.java
+++ src/Main.java
@@ -15,29 +15,5 @@ public class Main {
             System.out.println(iterator.current());
             iterator.next();
         }
-        // State Pattern
-//        Canvas canvas = new Canvas();
-//
-//        canvas.setCurrentTool(new EraserTool());
-//        canvas.mouseDown();
-//        canvas.mouseUp();
-//
-//        canvas.setCurrentTool(new BrushTool());
-//        canvas.mouseDown();
-//        canvas.mouseUp();
-        // Memento Pattern
-//        var editor = new Editor();
-//        var history = new History();
-//
-//        editor.setContent("a");
-//        history.push(editor.createState());
-//
-//        editor.setContent("b");
-//        history.push(editor.createState());
-//
-//        editor.setContent("c");
-//        editor.restore(history.pop());
-//
-//        System.out.println(editor.getContent());
     }
 }
